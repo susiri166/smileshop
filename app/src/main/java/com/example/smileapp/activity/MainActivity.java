7008cfcb7d2e19785f90fe0a803956efb9aec1bb
susiri166 <sdayan71@gmail.com>
2022-10-05 07:11:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/smileapp/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.smileapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smileapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
